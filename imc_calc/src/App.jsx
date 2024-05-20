c87560698a8368202d514fe6747776ee9e4136d9
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-05-20 15:21:59 -0300

    imc

---
+++ imc_calc/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ImcCalc from './components/ImcCalc.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <h2>IMG Calc</h2>
+      <ImcCalc />
+      </div>
+    </>
+  )
+}
+
+export default App
